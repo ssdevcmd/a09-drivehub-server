d9fd5f53566756d3f44f4d7fcf8abd265446887b
ssdevcmd <ssph079@gmail.com>
2026-06-21 09:15:22 +0600

    add post method to add car data

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
-const app = express()
 
+const app = express()
 const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -18,14 +22,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db('drivehub')
+    const carCollection = db.collection('cars')
+
+    app.post('/car', async (req, res) => {
+      const carData = req.body
+      console.log(carData);
+      const result = await carCollection.insertOne(carData)
+
+      res.json(result)
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
