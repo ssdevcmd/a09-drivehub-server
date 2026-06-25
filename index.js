ae8e58633aaaf9167f27f06fd967ffc9b66f5feb
ssdevcmd <ssph079@gmail.com>
2026-06-25 11:31:01 +0600

    make DELETE API to delete bookings car from my-bookings

--- index.js
+++ index.js
@@ -71,9 +71,24 @@ async function run() {
       res.json(result)
     });
 
+    app.get('/booking/:userId', async (req, res) => {
+      const {userId} = req.params
+
+      const result = await bookingCollection.find({userId}).toArray()
+
+      res.json(result)
+    })
+
     app.post('/booking', async (req, res) => {
       const bookingData = req.body;
       const result = await bookingCollection.insertOne(bookingData)
+
+      res.json(result)
+    });
+
+    app.delete('/booking/:bookingId', async (req, res) => {
+      const {bookingId} = req.params;
+      const result = await bookingCollection.deleteOne({_id: new ObjectId(bookingId)})
 
       res.json(result)
     })
